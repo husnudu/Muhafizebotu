448d31e7a59e39df1eb8e58f0f64be4fa50a38ff
husnudu <99674788+husnudu@users.noreply.github.com>
2022-03-18 11:12:57 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from LaylaRobot.sample_config import Config
 
 class Development(Config):
-    OWNER_ID = 1799203251  # your telegram ID
-    OWNER_USERNAME = "SpikerBey"  # your telegram username
-    API_KEY = "1842730159:AAHDQDQvWTm6a4Mwvvj0NQfFdLnPVqaZPAE"  # your api key, as provided by the @botfather
+    OWNER_ID = 5200193996  # your telegram ID
+    OWNER_USERNAME = "slmmnhusnu"  # your telegram username
+    API_KEY = "5118244333:AAEF80E3lszb2hVLkl4mZ1gVEkP9aPPKmaI"  # your api key, as provided by the @botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
-    MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
+    MESSAGE_DUMP = '-407527950' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
-    SUDO_USERS = [1816126399, 1108583389]  # List of id's for users which have sudo access to the bot.
+    SUDO_USERS = [5283911733]  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = ['translation']
